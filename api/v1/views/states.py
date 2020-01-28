0e4fa99430f4b571669bc24cef563e45d012cf3a
David Rosero C <davidroserocalle@gmail.com>
2020-01-28 17:23:53 +0000

    update file states.py, return the json for the api /api/v1/states

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -2,10 +2,15 @@
 from flask import jsonify
 from api.v1.views import app_views
 from models import storage
+from models.state import State
 
 
-@app_views.route('/states', methods=['GET'])
+@app_views.route('/states', methods=['GET'], strict_slashes=False)
 def view_states():
     """ Retrieves the list of all State objects """
-    states_obj = storage.all("State").values().to_dict()
-    return states_obj
+    new_list = []
+    states_obj = storage.all("State")
+    for data in states_obj.values():
+        new_list.append(data.to_dict())
+
+    return jsonify(new_list)
