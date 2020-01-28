6c667b3f32360533d9dd23459bd271dc650deca9
dneosphere <daniela.gomezp05@outlook.com>
2020-01-28 19:22:13 +0000

    Feature: POST request, not working

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -5,15 +5,26 @@ from models import storage
 from models.state import State
 
 
-@app_views.route('/states', methods=['GET'], strict_slashes=False)
+@app_views.route('/states', methods=['GET', 'POST'], strict_slashes=False)
 def view_states():
     """ Retrieves the list of all State objects """
-    new_list = []
-    states_obj = storage.all("State")
-    for data in states_obj.values():
-        new_list.append(data.to_dict())
+    if request.method == 'GET':
+        new_list = []
+        states_obj = storage.all("State")
+        for data in states_obj.values():
+            new_list.append(data.to_dict())
 
-    return jsonify(new_list)
+        return jsonify(new_list)
+
+    if request.method == 'POST':
+        if not 'name' in request.get_json():
+            abort(400, "Missing name")
+        if not request.get_json():
+            abort(400, "Not a JSON")
+
+        new_state = State(request.get_json())
+        new_state.save()
+        return jsonify(new_state.to_dict())
 
 
 @app_views.route('/states/<state_id>', strict_slashes=False,
@@ -27,6 +38,7 @@ def view_state_id(state_id):
                 id_found = state.to_dict()
                 return jsonify(id_found)
         abort(404)
+
     if request.method == 'DELETE':
         for state in states_obj.values():
             if state.id == state_id:
